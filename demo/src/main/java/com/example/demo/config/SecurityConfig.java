e78cf63cc4072baeebdb7988b432b75ac3ff4f05
chdas-programmer <chanchaldasprogrammer@gmail.com>
2026-03-23 15:50:21 +0530

    resolved the cors issue

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -26,10 +26,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http,
+
                                            JwtAuthenticationFilter jwtFilter)
             throws Exception {
 
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
@@ -77,6 +79,24 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+        @Bean
+        public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+
+        org.springframework.web.cors.CorsConfiguration configuration =
+                new org.springframework.web.cors.CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(java.util.List.of("*")); // 👈 HERE
+        configuration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(java.util.List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource source =
+                new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+        }
 }
 
 
